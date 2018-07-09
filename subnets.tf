4e6e7583e345aedfc63c8dd885db67ffa529bd92
Anup Das Gupta <asteranup@gmail.com>
2018-07-09 14:41:07 +0530

    Select availablity zone based on given region

--- subnets.tf
+++ subnets.tf
@@ -1,20 +1,22 @@
+data "aws_availability_zones" "available" {}
+
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${aws_vpc.mainvpc.id}"
   map_public_ip_on_launch = true
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
   cidr_block              = "10.0.3.0/24"
 
   tags = {
-    Name = "${var.aws_environment}-sn-public-10.0.3.0-ap-southeast-1a"
+    Name = "${var.aws_environment}-sn-public-10.0.3.0"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id            = "${aws_vpc.mainvpc.id}"
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "${data.aws_availability_zones.available.names[1]}"
   cidr_block        = "10.0.4.0/24"
 
   tags = {
-    Name = "${var.aws_environment}-sn-private-10.0.4.0-ap-southeast-1b"
+    Name = "${var.aws_environment}-sn-private-10.0.4.0"
   }
 }
